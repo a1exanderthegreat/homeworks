a8bb87f43df183c41f6e4dff8b18d39127a95eb9
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 22:37:21 +0400

    10th commit

---
+++ homework.py
@@ -0,0 +1,13 @@
+##shevkmnat sia chveni saxelisgan da gvarisgan da davprintot xmovnebis raodenoba
+
+
+full_name = "Aleksandre Grdzelidze"
+
+xmovnebis_raodenoba = 0
+
+for i in range(len(full_name)):
+    char = (full_name[i])
+    if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+        xmovnebis_raodenoba += 1      #xmovnebis_raodenoba = xmovnebis_raodenoba + 1 
+
+print(xmovnebis_raodenoba)
---
+++ homework1.py
@@ -0,0 +1,4 @@
+family = ["Aleksandre" , "Gulo" , "Giorgi" , "Grdzelidze" , "Xarazi" , "Grdzelidze"]
+print(family[0] , family[3])
+print(family[1] , family[4])
+print(family[2] , family[5])
