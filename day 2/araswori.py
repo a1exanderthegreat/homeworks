fb79bc5cd423b4ef147ded34dde64fa890fa4365
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 22:04:11 +0400

    fifth commit

---
+++ day 2/araswori.py
@@ -0,0 +1,15 @@
+# na me = Aleksandre
+# su rname = Grdzelidze"
+# a ge = "16 almost
+# ci ty = (kobuketi)
+# favo rite_fo od = "piza
+# goa = go at)
+# hei ght - 17 5
+# country = georgia"
+# money = 20 gela
+# print h ello
+# tempera ture = 100c"
+# weather -> "rain"            
+# pr int(sadjafik * su rname)                 
+# pr int(na% %%%%%me + age)
+# pr int na %me + surna mep rint(age % f avori te_foo d) 
---
+++ day 4/homework.py
@@ -0,0 +1,6 @@
+name = input("Enter Your Name : ")
+surname = input("Enter Your Surname : ")
+country_born = input("Where were you born? : ")
+age = int(input("Enter your age : "))
+height = input("Enter your height : ")
+print("Your name is:" + name , surname + " ", "You were born in:" + country_born + " " , "You are :" + " " + str(age) + " Years Old" + "," "You are :" + " " + height + "cm.")
