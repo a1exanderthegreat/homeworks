54573c53867aa02979d070e3606b82403031e111
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 22:44:03 +0400

    10th commit'

---
+++ day 10/homework.py
@@ -0,0 +1,12 @@
+#shekmenit sia da gvarisgan daprintet xmovnebis raodenoba
+
+full_name = "ალექსანდრე გრძელიძე"
+
+Consonant_count = 0
+
+for i in range(len(full_name)):
+    char = (full_name[i])
+    if char == "ბ" or char == "გ" or char == "დ" or char == "ვ" or char == "ზ" or char == "თ" or char == "კ" or char == "ლ" or char == "მ" or char == "ნ" or char == "პ" or char == "ჟ" or char == "რ" or char == "ს" or char == "ტ" or char == "ფ" or char == "ქ" or char == "ღ" or char == "ყ" or char == "შ" or char == "ჩ" or char == "ც" or char == "ძ" or char == "წ" or char == "ჭ" or char == "ხ" or char == "ჯ" or char == "ჰ":
+        Consonant_count += 1      #vowel_count = vowel_count + 1 
+
+print("Tanxmovani =" + " " + Consonant_count)
