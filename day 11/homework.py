ec14499b9c126b146e863b6ebbc339de5209e3b7
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 22:50:41 +0400

    11th commit

---
+++ day 11/homework.py
@@ -0,0 +1,10 @@
+fav_food = ["Pizza" , "Shaurma" , "Mwvadi" , "Yiyliyo"]
+print(fav_food[0:])
+print(fav_food[0] , fav_food[3])
+print(fav_food[1] , fav_food[2])
+
+
+fav_colours = ["Black" , "Grey" , "Purple"]
+print(fav_colours[0:])
+print(fav_colours[0:2])
+print(fav_colours[2:])
