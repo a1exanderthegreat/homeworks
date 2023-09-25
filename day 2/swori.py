725aa995b373f0754461314a1cda074b3005b0f7
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 21:52:31 +0400

    third commit

---
+++ day 2/swori.py
@@ -0,0 +1,20 @@
+name = "Aleksandre"
+surname = "Grdzelidze"
+age = 15
+city = "Kobuleti"
+favorite_food = "Pizza"
+goa = "GOA-T"
+height = 175
+country = "georgia"
+money = "20 Gela"
+temperature = "100c"
+weather = "SICXE"
+
+print("hello" + name)
+print(weather)
+print(temperature)
+print(money)
+print(goa)
+print(favorite_food)
+print(name , surname , height , money)
+print(country , favorite_food)
