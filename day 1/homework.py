e70df91b435d9fc1113ef70b6676728cff9178d5
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 21:40:26 +0400

    second commit

---
+++ day 1/homework.py
@@ -0,0 +1,9 @@
+
+name = "Aleksandre"
+surname = "Grdzelidze"
+age = 15
+fav_food = "lobiani"
+height = 175
+weight = 60
+
+print(name + " " + surname + " " + str(age) + " " + fav_food + " " + str(height) + " " + str(weight))
---
+++ day 1/homework1.py
@@ -0,0 +1,10 @@
+name = "Aleksandre"
+surname = "Grdzelidze"
+height = 175
+weight = 60
+bench = 0
+deadlift = 1
+
+print(height / weight)
+print(bench + deadlift)
+print(name + " " + surname)
