910157dfa23c2c71357fa98e8a49acae4ef196ab
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 22:10:41 +0400

    sixth commit

---
+++ day 5/homework.py
@@ -0,0 +1,4 @@
+# 1. momxmarebels sheekitxet tavisi asaki da aris tu ara is 18< (True / False)
+age = int(input("Enter your age : "))
+print(age < 18)     
+
---
+++ day 5/homework1.py
@@ -0,0 +1,5 @@
+# 2. momxmarebels sheekitxet tavisi da mamamisis asaki da daprintet mtlianad(jamshi) 50< aris tu ara 
+
+son_age = int(input("Enter sons age : "))
+father_age = int(input("Enter fathers age : "))
+print(son_age + father_age < 50)      
