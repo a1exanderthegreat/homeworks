1e33aab1d8cc3ee2e4aaabd58a5538b127a61745
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 22:29:02 +0400

    9th commit

---
+++ day 8/homework.py
@@ -0,0 +1,7 @@
+#daprintet 1-100 mde cifrebi da miuweret kentia tu luwi
+
+for i in range(2,100,2):
+    print(i , "Luwi")
+
+for j in range(1,100,2):
+    print(j , "Kenti") 
---
+++ day 8/homework1.py
@@ -0,0 +1,15 @@
+#tu momxmarebeli aris mamakaci da 65+ print gekutvnit pensia tu momxmarebeli aris kali da 60+ daprintet gekutvnit pensia tu aris nonbinary print go kys
+
+
+age_of_user = int(input("Enter your age : "))
+user_gender = input("Enter your sex (Male,Female OR Nonbinary) : ")
+
+
+if age_of_user >= 65 and user_gender == ("Male"):
+    print("Gilocav Shen Gekutvnis Pensia :3 ")
+else:
+    if age_of_user >= 60 and user_gender == ("Female"):
+        print("Gilocav Shen Gekutvnis Pensiaa :33")
+
+    if user_gender == ("Nonbinary"):
+        print("Bro's nonbinary n wants pension bro go kys")
