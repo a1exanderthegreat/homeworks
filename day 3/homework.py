5c95fdc0f024e7f894597cb18f8a8d6d3cc65a37
a1exanderthegreat <aleksandregrdzelidze02@gmail.com>
2023-09-25 21:58:48 +0400

    fourth commit

---
+++ day 3/homework.py
@@ -0,0 +1,4 @@
+age = int(input("Enter Your Age : "))
+time_travel = int(input("How many years would you like to time travel? : "))
+age_after_time_travel = age + time_travel
+print("Age after time travel =  " + str(age_after_time_travel))
